d894f23e3970617e8b15e3497d9c19135adf140d
HuXin0817 <202219120810@stu.cdut.edu.cn>
2024-10-01 21:23:36 +0800

    finish IndexMeta::to_string, IndexMeta::to_json, IndexMeta::from_json

--- src/observer/storage/index/index_meta.cpp
+++ src/observer/storage/index/index_meta.cpp
@@ -27,17 +27,60 @@ RC IndexMeta::init(const char *name, const vector<FieldMeta> &fields)
 string IndexMeta::to_string() const
 {
   std::ostringstream oss;
-  // TODO
+  oss << "Index Name: " << name_ << ", Fields: [";
+  for (size_t i = 0; i < fields_.size(); ++i) {
+    oss << fields_[i].name();
+    if (i < fields_.size() - 1) {
+      oss << ", ";
+    }
+  }
+  oss << "], Total Length: " << fields_total_len_;
   return oss.str();
 }
 
 void IndexMeta::to_json(Json::Value &json_value) const
 {
-  // TODO
+  json_value["name"] = name_;
+  json_value["fields_total_len"] = fields_total_len_;
+
+  Json::Value fields_json(Json::arrayValue);
+  for (const auto &field : fields_) {
+    Json::Value field_json;
+    field.to_json(field_json);
+    fields_json.append(field_json);
+  }
+  json_value["fields"] = fields_json;
+
+  Json::Value offsets_json(Json::arrayValue);
+  for (const auto &offset : fields_offset_) {
+    offsets_json.append(offset);
+  }
+  json_value["fields_offset"] = offsets_json;
 }
 
 RC IndexMeta::from_json(const Json::Value &json_value, IndexMeta &index)
 {
-  // TODO
+  if (!json_value.isMember("name") || !json_value.isMember("fields") || !json_value.isMember("fields_total_len") || !json_value.isMember("fields_offset")) {
+    return RC::INVALID_ARGUMENT;
+  }
+
+  index.name_ = json_value["name"].asString();
+  index.fields_total_len_ = json_value["fields_total_len"].asInt();
+
+  const Json::Value &fields_json = json_value["fields"];
+  for (const auto &field_json : fields_json) {
+    FieldMeta field;
+    RC rc = FieldMeta::from_json(field_json, field);
+    if (rc != RC::SUCCESS) {
+      return rc;
+    }
+    index.fields_.push_back(field);
+  }
+
+  const Json::Value &offsets_json = json_value["fields_offset"];
+  for (const auto &offset : offsets_json) {
+    index.fields_offset_.push_back(offset.asInt());
+  }
+
   return RC::SUCCESS;
 }
