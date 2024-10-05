c37c6d722d2c836c645bca0ff6c74dcfb0610a30
HuXin0817 <202219120810@stu.cdut.edu.cn>
2024-10-05 22:06:43 +0800

    完成DATE_FORMAT

--- src/observer/sql/expr/function.h
+++ src/observer/sql/expr/function.h
@@ -56,14 +56,67 @@ static inline RC DATE_FORMAT(const vector<Value> &args, Value &result)
   if (args.size() != 2) {
     return RC::INVALID_ARGUMENT;
   }
-  if (args[0].attr_type() != AttrType::DATES) {
+  if (args[0].attr_type() != AttrType::DATES && args[0].attr_type() != AttrType::CHARS) {
     return RC::INVALID_ARGUMENT;
   }
   if (args[1].attr_type() != AttrType::CHARS) {
     return RC::INVALID_ARGUMENT;
   }
 
-  // TODO: DATE_FORMAT
+  int year, month, day;
+
+  if (args[0].attr_type() == AttrType::DATES) {
+    // 提取年、月、日（假设日期格式为YYYYMMDD）
+    int val = args[0].get_int();
+    year    = val / 10000;        // 获取年份
+    month   = (val / 100) % 100;  // 获取月份
+    day     = val % 100;          // 获取日期
+  }
+
+  if (args[0].attr_type() == AttrType::CHARS) {
+    // 日期格式假设为 '2019-9-17' 或 '2019-09-17'
+    std::string date_str = args[0].to_string();
+    sscanf(date_str.c_str(), "%d-%d-%d", &year, &month, &day);
+  }
+
+  auto fmt = args[1].to_string();
+
+  string str;
+
+  // 遍历格式字符串，并替换格式符
+  for (size_t i = 0; i < fmt.length(); ++i) {
+    if (fmt[i] == '%' && i + 1 < fmt.length()) {
+      switch (fmt[i + 1]) {
+        case 'Y':  // 四位数年份
+          str += std::to_string(year);
+          break;
+        case 'y':  // 两位数年份
+          str += std::to_string(year).substr(2, 2);
+          break;
+        case 'm':  // 两位数月份
+          str += (month < 10 ? "0" : "") + std::to_string(month);
+          break;
+        case 'c':  // 不带前导零的月份
+          str += std::to_string(month);
+          break;
+        case 'd':  // 两位数日期
+          str += (day < 10 ? "0" : "") + std::to_string(day);
+          break;
+        case 'e':  // 不带前导零的日期
+          str += std::to_string(day);
+          break;
+        default:  // 未知格式符，按原样输出
+          str += fmt[i];
+          str += fmt[i + 1];
+          break;
+      }
+      ++i;  // 跳过格式符的下一个字符
+    } else {
+      str += fmt[i];  // 普通字符直接追加
+    }
+  }
+
+  result = Value(str.c_str());
   return RC::SUCCESS;
 }
 
