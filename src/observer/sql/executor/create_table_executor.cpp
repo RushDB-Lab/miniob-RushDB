4a0d164706879c4b4187766fa7de1b2ca6e4caa8
HuXin0817 <202219120810@stu.cdut.edu.cn>
2024-10-10 18:15:06 +0800

    完成attr_infos推断

--- src/observer/sql/executor/create_table_executor.cpp
+++ src/observer/sql/executor/create_table_executor.cpp
@@ -23,6 +23,7 @@ See the Mulan PSL v2 for more details. */
 
 RC CreateTableExecutor::execute(SQLStageEvent *sql_event)
 {
+  RC       rc;
   Stmt    *stmt    = sql_event->stmt();
   Session *session = sql_event->session_event()->session();
   ASSERT(stmt->type() == StmtType::CREATE_TABLE,
@@ -30,10 +31,45 @@ RC CreateTableExecutor::execute(SQLStageEvent *sql_event)
       static_cast<int>(stmt->type()));
 
   CreateTableStmt *create_table_stmt = static_cast<CreateTableStmt *>(stmt);
+  const char      *table_name        = create_table_stmt->table_name().c_str();
+  if (create_table_stmt->create_table_select_stmt()) {
+    std::vector<FieldExpr *>   field_exprs;
+    std::unordered_set<string> table_names;
+    for (auto &expr : create_table_stmt->create_table_select_stmt()->query_expressions()) {
+      auto field_expr = dynamic_cast<FieldExpr *>(expr.get());
+      if (field_expr) {
+        table_names.emplace(field_expr->table_name());
+        field_exprs.push_back(field_expr);
+      }
+    }
 
-  const char *table_name = create_table_stmt->table_name().c_str();
-  RC          rc         = session->get_current_db()->create_table(
-      table_name, create_table_stmt->attr_infos(), create_table_stmt->storage_format());
+    std::vector<AttrInfoSqlNode> attr_infos;
+    for (auto &expr : field_exprs) {
+      AttrInfoSqlNode attr_info;
+      if (table_names.size() == 1) {
+        attr_info.name = expr->field_name();
+      } else {
+        attr_info.name = expr->table_name();
+        attr_info.name += ".";
+        attr_info.name += expr->field_name();
+      }
+      attr_info.length   = expr->field().meta()->len();
+      attr_info.type     = expr->field().meta()->type();
+      attr_info.nullable = expr->field().meta()->nullable();
+
+      attr_infos.push_back(attr_info);
+    }
+
+    rc = session->get_current_db()->create_table(table_name, attr_infos, create_table_stmt->storage_format());
+    if (OB_FAIL(rc)) {
+      return rc;
+    }
+
+    // TODO: insert records
+  } else {
+    rc = session->get_current_db()->create_table(
+        table_name, create_table_stmt->attr_infos(), create_table_stmt->storage_format());
+  }
 
   return rc;
 }
