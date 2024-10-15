50c7f2ca718432d3d5aa45944f9e0aa84119f075
HuXin0817 <202219120810@stu.cdut.edu.cn>
2024-10-15 14:24:16 +0800

    完成vector

--- test.sql
+++
@@ -1,9 +0,0 @@
-create table a (v vector(3) nullable);
-
-insert into a values ('[1]');
-insert into a values ('[1, 2]');
-insert into a values ('[1, 2, 3]');
-insert into a values ('[1, 2, 3, 4]');
-insert into a values (null);
-
-select * from a;
